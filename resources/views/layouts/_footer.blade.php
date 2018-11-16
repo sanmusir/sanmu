778eaacd93b109aed6a9a1cc35de79fcf052ea1b
vagrant <vagrant@homestead>
2018-11-16 06:29:26 +0000

    修改配置

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="container">
+        <div>
+            <p class="phrase">刻意练习，持续分享</p>
+        </div>
+
+        <div>
+            <p class="pull-left">©2018 power by sanmu</p>
+            <p class="pull-right footerlink"><span style="color: #e27575;font-size: 14px;"> ❤</span><a href="https://github.com/sanmusir">Github</a></p>
+            <p class="pull-right footerlink"><span style="color: #e27575;font-size: 14px;"> ❤</span><a href="http://rabbitmm.com/">博客</a></p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ config('locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title','SanMu') - 刻意练习</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+
+        @include('layouts._header')
+
+        <div class="container">
+            @yield('content')
+        </div>
+
+        @include('layouts._footer')
+
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
